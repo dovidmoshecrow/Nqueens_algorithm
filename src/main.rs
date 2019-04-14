275180f3d388eb6629decc6be812cdd902afdfa4
dovid moshe crow <dovidmoshecrowpc@outlook.com>
2019-04-14 10:08:24 +0300

    First commit

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,42 @@
+const N: i32 = 20;
 fn main() {
-  let mut grid :[[bool;8];8] = [[false;8];8];
-  if !minimax(&mut grid,0){
-      println!("No solotion");  
-  }else {
-       for e in grid.iter(){
-           println!("{:?}",e);
-       }
-  }
+    let mut grid: [[bool; N as usize]; N as usize] = [[false; N as usize]; N as usize];
+    if !find_nqueens(&mut grid, 0) {
+        println!("No solotion");
+    } else {
+        for e in grid.iter() {
+            println!("{:?}", e);
+        }
+    }
 }
 
-fn minimax(grid:&mut [[bool; 8]; 8],col:i32) -> bool{
-   if col >= 8{
-       return true;
-   };
-   for row in 0..8{
-     if check_pos(grid,row,col) {
-       grid[row as usize][col as usize] = true;
-     if minimax(grid,col + 1){
+fn find_nqueens(grid: &mut [[bool; N as usize]; N as usize], col: i32) -> bool {
+    if col >= N {
         return true;
-     };
-     grid[row as usize][col as usize] = false;
-   };
-   };
-   false
+    };
+    for row in 0..N {
+        if check_pos(grid, row, col) {
+            grid[row as usize][col as usize] = true;
+            if find_nqueens(grid, col + 1) {
+                return true;
+            };
+            grid[row as usize][col as usize] = false;
+        };
+    }
+    false
 }
 
-
-
-fn check_pos(grid:&[[bool;8];8],row:i32,col:i32)-> bool {
-   for i in 0..8 {
-       if grid[row as usize][i] {
-          return false;
-       }
-   };
-   for i in 0..8 {
-       if grid[i][col as usize] {
-           return false;
-       }
-   };
-   let a = row + col;
-   let b = row - col;
-   for x in 0..8{
-       for y in 0..8{
-             if x + y == a || x - y == b{
-                if grid[x as usize][y as usize]{
+fn check_pos(grid: &[[bool; N as usize]; N as usize], row: i32, col: i32) -> bool {
+    let a = row + col;
+    let b = row - col;
+    for x in 0..N {
+        for y in 0..N {
+            if x + y == a || x - y == b || x == row || y == col {
+                if grid[x as usize][y as usize] {
                     return false;
                 }
-             };
-       };
-   };
-   true
+            };
+        }
+    }
+    true
 }
